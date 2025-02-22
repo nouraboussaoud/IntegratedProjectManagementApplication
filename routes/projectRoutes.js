436ab4035f6037e7c3e69b4d777319f9cdef515b
Yessine Ben Hamroun <yessine.benhamroun@esprit.tn>
2025-02-22 18:28:19 +0100

    CRUD Implementation of Group, Project and Task Models

---
+++ routes/projectRoutes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const {
+    getAllProjects,
+    getProjectById,
+    getProjectByName,
+    getProjectByGroup,
+    getProjectTasks,
+    getProjectFinishedTasks,
+    getProjectPendingTasks,
+    getProjectInProgressTasks,
+    createProject,
+    updateProject,
+    deleteProject,
+    addTask,
+    assignProjectToGroup,
+} = require("../controllers/projectController");
+
+router.get("/getAllProjects", getAllProjects);
+router.get("/getProjectById/:id", getProjectById);
+router.get("/getProjectByName/:name", getProjectByName);
+router.get("/getProjectByGroup/:id", getProjectByGroup);
+router.get("/getProjectTasks/:id", getProjectTasks);
+router.get("/getProjectFinishedTasks/:id", getProjectFinishedTasks);
+router.get("/getProjectPendingTasks/:id", getProjectPendingTasks);
+router.get("/getProjectInProgressTasks/:id", getProjectInProgressTasks);
+router.post("/createProject", createProject);
+router.put("/updateProject/:id", updateProject);
+router.delete("/deleteProject/:id", deleteProject);
+router.put("/addTask/:id", addTask);
+router.put("/assignProjectToGroup/:id", assignProjectToGroup);
